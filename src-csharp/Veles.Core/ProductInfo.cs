8008e11bf985b96f43afb9dcd1c47c9eaca17b6a
kutsandriy14-cyber <2.56403765e+08+kutsandriy14-cyber@users.noreply.github.com>
2026-08-27 14:00:34 +0000

    Bump launcher to v0.1.9

--- src-csharp/Veles.Core/ProductInfo.cs
+++ src-csharp/Veles.Core/ProductInfo.cs
@@ -4,7 +4,7 @@ namespace Veles.Core
 {
     public static class ProductInfo
     {
-        public const string VersionText = "0.1.8";
+        public const string VersionText = "0.1.9";
         public static Version Version { get { return new Version(VersionText); } }
     }
 }
